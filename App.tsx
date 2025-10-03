c08a90e8fe71e307a0dc9bb3688f5237f335150d
eva <83175995+evapopp@users.noreply.github.com>
2025-10-03 16:20:48 -0700

    clean up directories

--- App.tsx
+++ App.tsx
@@ -1,18 +1,1 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { Text, View } from 'react-native';
-// import useGlobalStyles from './app/styles/UseGlobalStyles';
-// import React from 'react';
-
-// export default function App() {
-//   const globalStyles = useGlobalStyles()
-
-//   return (
-//     <View style={globalStyles.container}>
-//       <Text >Requited</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
 import "expo-router/entry";
-
